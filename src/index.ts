e36ae41409bc36be085e65f6b577ebb88dea26d0
sk-buy <sk-buy@users.noreply.github.com>
2026-05-23 22:56:57 +0800

    Use standard MD5 library for EPay signing

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import { md5 } from "js-md5";
+
 export interface Env {
   EPAY_PID: string;
   EPAY_KEY: string;
@@ -305,110 +307,13 @@ function normalizePayment(provider: string, payload: ProviderPayload): Normalize
   };
 }
 
-function md5Hex(input: string) {
-  function add32(a: number, b: number) {
-    return (a + b) & 0xffffffff;
-  }
-  function rol(num: number, cnt: number) {
-    return (num << cnt) | (num >>> (32 - cnt));
-  }
-  function cmn(q: number, a: number, b: number, x: number, s: number, t: number) {
-    return add32(rol(add32(add32(a, q), add32(x, t)), s), b);
-  }
-  function ff(a: number, b: number, c: number, d: number, x: number, s: number, t: number) {
-    return cmn((b & c) | (~b & d), a, b, x, s, t);
-  }
-  function gg(a: number, b: number, c: number, d: number, x: number, s: number, t: number) {
-    return cmn((b & d) | (c & ~d), a, b, x, s, t);
-  }
-  function hh(a: number, b: number, c: number, d: number, x: number, s: number, t: number) {
-    return cmn(b ^ c ^ d, a, b, x, s, t);
-  }
-  function ii(a: number, b: number, c: number, d: number, x: number, s: number, t: number) {
-    return cmn(c ^ (b | ~d), a, b, x, s, t);
-  }
-  function bytesToWords(value: string) {
-    const bytes = new TextEncoder().encode(value);
-    const words: number[] = [];
-    for (let index = 0; index < bytes.length; index += 1) {
-      words[index >> 2] |= bytes[index] << ((index % 4) << 3);
-    }
-    words[bytes.length >> 2] |= 0x80 << ((bytes.length % 4) << 3);
-    words[(((bytes.length + 8) >> 6) << 4) + 14] = bytes.length * 8;
-    return words;
-  }
-  function hex(value: number) {
-    let output = "";
-    for (let index = 0; index < 4; index += 1) {
-      output += ((value >> (index * 8 + 4)) & 0x0f).toString(16) + ((value >> (index * 8)) & 0x0f).toString(16);
-    }
-    return output;
-  }
-
-  const words = bytesToWords(input);
-  let a = 1732584193;
-  let b = -271733879;
-  let c = -1732584194;
-  let d = 271733878;
-
-  for (let i = 0; i < words.length; i += 16) {
-    const olda = a;
-    const oldb = b;
-    const oldc = c;
-    const oldd = d;
-
-    a = ff(a, b, c, d, words[i], 7, -680876936); d = ff(d, a, b, c, words[i + 1], 12, -389564586);
-    c = ff(c, d, a, b, words[i + 2], 17, 606105819); b = ff(b, c, d, a, words[i + 3], 22, -1044525330);
-    a = ff(a, b, c, d, words[i + 4], 7, -176418897); d = ff(d, a, b, c, words[i + 5], 12, 1200080426);
-    c = ff(c, d, a, b, words[i + 6], 17, -1473231341); b = ff(b, c, d, a, words[i + 7], 22, -45705983);
-    a = ff(a, b, c, d, words[i + 8], 7, 1770035416); d = ff(d, a, b, c, words[i + 9], 12, -1958414417);
-    c = ff(c, d, a, b, words[i + 10], 17, -42063); b = ff(b, c, d, a, words[i + 11], 22, -1990404162);
-    a = ff(a, b, c, d, words[i + 12], 7, 1804603682); d = ff(d, a, b, c, words[i + 13], 12, -40341101);
-    c = ff(c, d, a, b, words[i + 14], 17, -1502002290); b = ff(b, c, d, a, words[i + 15], 22, 1236535329);
-
-    a = gg(a, b, c, d, words[i + 1], 5, -165796510); d = gg(d, a, b, c, words[i + 6], 9, -1069501632);
-    c = gg(c, d, a, b, words[i + 11], 14, 643717713); b = gg(b, c, d, a, words[i], 20, -373897302);
-    a = gg(a, b, c, d, words[i + 5], 5, -701558691); d = gg(d, a, b, c, words[i + 10], 9, 38016083);
-    c = gg(c, d, a, b, words[i + 15], 14, -660478335); b = gg(b, c, d, a, words[i + 4], 20, -405537848);
-    a = gg(a, b, c, d, words[i + 9], 5, 568446438); d = gg(d, a, b, c, words[i + 14], 9, -1019803690);
-    c = gg(c, d, a, b, words[i + 3], 14, -187363961); b = gg(b, c, d, a, words[i + 8], 20, 1163531501);
-    a = gg(a, b, c, d, words[i + 13], 5, -1444681467); d = gg(d, a, b, c, words[i + 2], 9, -51403784);
-    c = gg(c, d, a, b, words[i + 7], 14, 1735328473); b = gg(b, c, d, a, words[i + 12], 20, -1926607734);
-
-    a = hh(a, b, c, d, words[i + 5], 4, -378558); d = hh(d, a, b, c, words[i + 8], 11, -2022574463);
-    c = hh(c, d, a, b, words[i + 11], 16, 1839030562); b = hh(b, c, d, a, words[i + 14], 23, -35309556);
-    a = hh(a, b, c, d, words[i + 1], 4, -1530992060); d = hh(d, a, b, c, words[i + 4], 11, 1272893353);
-    c = hh(c, d, a, b, words[i + 7], 16, -155497632); b = hh(b, c, d, a, words[i + 10], 23, -1094730640);
-    a = hh(a, b, c, d, words[i + 13], 4, 681279174); d = hh(d, a, b, c, words[i], 11, -358537222);
-    c = hh(c, d, a, b, words[i + 3], 16, -722521979); b = hh(b, c, d, a, words[i + 6], 23, 76029189);
-    a = hh(a, b, c, d, words[i + 9], 4, -640364487); d = hh(d, a, b, c, words[i + 12], 11, -421815835);
-    c = hh(c, d, a, b, words[i + 15], 16, 530742520); b = hh(b, c, d, a, words[i + 2], 23, -995338651);
-
-    a = ii(a, b, c, d, words[i], 6, -198630844); d = ii(d, a, b, c, words[i + 7], 10, 1126891415);
-    c = ii(c, d, a, b, words[i + 14], 15, -1416354905); b = ii(b, c, d, a, words[i + 5], 21, -57434055);
-    a = ii(a, b, c, d, words[i + 12], 6, 1700485571); d = ii(d, a, b, c, words[i + 3], 10, -1894986606);
-    c = ii(c, d, a, b, words[i + 10], 15, -1051523); b = ii(b, c, d, a, words[i + 1], 21, -2054922799);
-    a = ii(a, b, c, d, words[i + 8], 6, 1873313359); d = ii(d, a, b, c, words[i + 15], 10, -30611744);
-    c = ii(c, d, a, b, words[i + 6], 15, -1560198380); b = ii(b, c, d, a, words[i + 13], 21, 1309151649);
-    a = ii(a, b, c, d, words[i + 4], 6, -145523070); d = ii(d, a, b, c, words[i + 11], 10, -1120210379);
-    c = ii(c, d, a, b, words[i + 2], 15, 718787259); b = ii(b, c, d, a, words[i + 9], 21, -343485551);
-
-    a = add32(a, olda);
-    b = add32(b, oldb);
-    c = add32(c, oldc);
-    d = add32(d, oldd);
-  }
-
-  return `${hex(a)}${hex(b)}${hex(c)}${hex(d)}`;
-}
-
 function buildEpaySignature(params: Record<string, string>, key: string) {
   const sorted = Object.entries(params)
     .filter(([name, value]) => value !== "" && name !== "sign" && name !== "sign_type")
     .sort(([a], [b]) => (a < b ? -1 : a > b ? 1 : 0))
     .map(([name, value]) => `${name}=${value}`)
     .join("&");
-  return md5Hex(`${sorted}${key}`);
+  return md5(`${sorted}${key}`);
 }
 
 function normalizeEpaySubmitUrl(paymentUrl: string) {
@@ -610,7 +515,7 @@ async function handleDebugSign(request: Request, env: Env) {
   return json({
     ok: true,
     sorted,
-    sign: md5Hex(`${sorted}${config.epayKey}`),
+    sign: md5(`${sorted}${config.epayKey}`),
     epayKeyPrefix: config.epayKey.slice(0, 5),
     epayKeySuffix: config.epayKey.slice(-5),
   });
